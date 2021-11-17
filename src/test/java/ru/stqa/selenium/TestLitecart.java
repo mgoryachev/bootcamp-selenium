8d90d8973da9f4324d5f818ebea02017c5b5be17
Максим Горячев <93838982+mgoryachev@users.noreply.github.com>
2021-11-17 21:56:02 +0500

    Задание 3
    Класс для проверки TestLitecart

--- src/test/java/ru/stqa/selenium/TestLitecart.java
+++ src/test/java/ru/stqa/selenium/TestLitecart.java
@@ -4,34 +4,37 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class TestSeleniumFirst {
+public class TestLitecart {
 
     private WebDriver driver;
     private WebDriverWait wait;
 
     @Before
-    public void start(){
+    public void start() {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 5);
     }
 
     @Test
-    public void myFirstTestSelenium(){
-        driver.get("https://google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver", Keys.ENTER);
-        wait.until(titleIs("webdriver - Поиск в Google"));
+    public void loginAdminTest() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
     }
 
     @After
-    public void stop(){
-        driver.quit();
+    public void stop() {
+        if (driver != null) {
+            driver.quit();
+        }
         driver = null;
     }
 }
